de86c106a4ad66e34d3ed3142a1eee0c86660bf2
Meyer <ehlmeyer@gmail.com>
2019-05-03 16:31:35 -0500

    axios GET request happening

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,38 @@
 import React, { Component } from 'react';
 import './App.css';
+import GalleryList from '../GalleryList/GalleryList'
+import axios from 'axios';
 
 class App extends Component {
+  state = {
+    galleryList: []
+  }
+
+  componentDidMount() {
+    //on first render, load gallery
+    this.getGalleryList();
+  }
+
+  // getGalleryList is a function that retrieves images from gallery.data.js and changes state
+  getGalleryList = ()=> {
+    // console.log('in getGalleryList');
+    //axios get gallery list from gallery.data.js
+    axios.get('/gallery')
+    .then(response => {
+      //test response.data
+      console.log(response.data);
+      //set state to update galleryList array with data from gallery.data
+      this.setState({
+        galleryList: response.data
+      })
+      
+    })//end axios get .then
+      .catch(error => {
+        console.log(error);
+      })//end axios get .catch
+  }
+
+
   render() {
     return (
       <div className="App">
@@ -9,8 +40,9 @@ class App extends Component {
           <h1 className="App-title">Gallery of my life</h1>
         </header>
         <br/>
+        <GalleryList />
         <p>Gallery goes here</p>
-        <img src="images/goat_small.jpg"/>
+        <img alt="A mountain goat at Glacier National Park."src="images/goat_small.jpg"/>
       </div>
     );
   }
