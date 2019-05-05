20555ab5e1cb85bb2f3a48329d4af1ac28914aff
Meyer <ehlmeyer@gmail.com>
2019-05-04 19:33:38 -0500

    submit button responds and clears inputs, but doesn't add image to database yet

--- src/components/GalleryForm/GalleryForm.js
+++ src/components/GalleryForm/GalleryForm.js
@@ -10,12 +10,38 @@ class GalleryForm extends Component {
             alt: ''
 
         }
-    }    
+    }
+    
+    handleChangeFor = (property) => {
+        return (event) => {
+            this.setState({
+                newGalleryItem: {
+                    ...this.state.newGalleryItem,
+                    [property]: event.target.value
+                }
+            })//end setState
+        }//end return
+    } //end handleChangeFor
+
+    handleSubmit = (event)=> {
+        event.preventDefault();
+        console.log('in handleSubmit');
+        this.setState({
+            newGalleryItem: {
+                path: '',
+                description: '',
+                alt: ''
+            }
+        })
+    }
+
     render(){
+        console.log(this.state.newGalleryItem.path);
+        
         return (
-            <form>
-                <input type="text" placeholder="Image Link" />
-                <input type="text" placeholder="Image Description"/>
+            <form onSubmit={this.handleSubmit}>
+                <input value={this.state.newGalleryItem.path} onChange={this.handleChangeFor('path')} type="text" placeholder="Image Link" />
+                <input value={this.state.newGalleryItem.description} onChange={this.handleChangeFor('description')} type="text" placeholder="Image Description"/>
                 <button type="submit">Submit</button>
             </form>
             // <p>form here</p>
