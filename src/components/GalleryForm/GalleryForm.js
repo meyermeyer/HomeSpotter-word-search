1ddc8969e6aed930af2fda341ce3bff55806bfb3
Meyer <ehlmeyer@gmail.com>
2019-05-06 00:22:31 -0500

    added some materialUI buttons

--- src/components/GalleryForm/GalleryForm.js
+++ src/components/GalleryForm/GalleryForm.js
@@ -1,4 +1,8 @@
 import React, { Component } from 'react';
+import Button from '@material-ui/core/Button';
+import Icon from '@material-ui/core/Icon';
+import DeleteIcon from '@material-ui/icons/Delete';
+import SaveIcon from '@material-ui/icons/Save';
 
 class GalleryForm extends Component {
 
@@ -43,7 +47,9 @@ class GalleryForm extends Component {
             <form onSubmit={this.handleSubmit}>
                 <input value={this.state.newGalleryItem.path} onChange={this.handleChangeFor('path')} type="text" placeholder="Image Link" />
                 <input value={this.state.newGalleryItem.description} onChange={this.handleChangeFor('description')} type="text" placeholder="Image Description"/>
-                <button type="submit">Submit</button>
+                <Button variant="contained" color="primary" size="small" type="submit">
+                    Submit
+                </Button>
             </form>
             // <p>form here</p>
         )
