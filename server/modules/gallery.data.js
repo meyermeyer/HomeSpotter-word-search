f133b46f8a6612ab98b0806cdae27e7c21823738
Meyer <ehlmeyer@gmail.com>
2019-08-01 17:54:00 -0500

    initial

--- server/modules/gallery.data.js
+++
@@ -1,8 +0,0 @@
-const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', alt:'Mountain goat in Glacier National Park.', likes: 0 },
-    { id: 2, path: 'images/Shrimp-tripping-1200x630.jpg', description: 'Image of a cooked shrimp in front of trippy, swirly background.', alt: 'Cooked shrimp in front of trippy, swirly background.',likes: 0 },
-    { id: 3, path: 'images/starnose.jpg', description: 'Photo of a terrifying star-nosed mole.', alt: 'terrifying star- nosed mole.', likes: 0 },
-    { id: 4, path: 'images/yeti-crab-photo-350.jpg', description: 'Photo of a fuzzy yeti crab.', alt: 'A fuzzy yeti crab.', likes: 0 }
-];
-
-module.exports = galleryItems;
--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,78 +1,11 @@
 import React, { Component } from 'react';
-import './App.css';
-import GalleryList from '../GalleryList/GalleryList'
-import GalleryForm from '../GalleryForm/GalleryForm'
-import axios from 'axios';
 
 class App extends Component {
-  state = {
-    galleryList: []
-  }
-
-  componentDidMount() {
-    //on first render, load gallery
-    this.getGalleryList();
-  }
-
-  // getGalleryList is a function that retrieves images from gallery.data.js and changes state
-  getGalleryList = ()=> {
-    // console.log('in getGalleryList');
-    //axios get gallery list from gallery.data.js
-    axios.get('/gallery')
-    .then(response => {
-      //test response.data
-      console.log(response.data);
-      //set state to update galleryList array with data from gallery.data
-      this.setState({
-        galleryList: response.data
-      })
-      
-    })//end axios get .then
-      .catch(error => {
-        console.log(error);
-      })//end axios get .catch
-  }
-
-  //newGalleryItem passed from GalleryList component
-  submitGalleryItem = (newGalleryItem) => {
-    console.log('in submitGalleryItem');
-    //axios POST route
-    axios.post('/gallery', newGalleryItem)
-    .then(response => {
-      console.log(response);
-      this.getGalleryList();
-    }) //end axios post /gallery
-    .catch(error => {
-      console.log('error in post', error); 
-    })//end .catch
-  };//end submitGalleryItem
-
+  
   render() {
     return (
       <>
-        <div className="App">
-          <header className="App-header">
-            <h1 className="App-title">Gallery of my life</h1>
-          </header>
-          <br/>
-          {/* pass submitGalleryItem function to GalleryForm */}
-          <GalleryForm submitGalleryItem={this.submitGalleryItem}/>
-          {/* pass getGalleryList function as a prop so child can send to next child to re-render like count after like click */}
-          <GalleryList getGalleryList={this.getGalleryList} galleryList={this.state.galleryList}/>
-          {/* <p>Gallery goes here</p> */}
-          {/* <img alt="A mountain goat at Glacier National Park."src="images/goat_small.jpg"/> */}
-        </div>
-        {/* <div>
-          <ReactMic
-            record={this.state.record}
-            className="sound-wave"
-            onStop={this.onStop}
-            onData={this.onData}
-            strokeColor="#000000"
-            backgroundColor="#FF4081" />
-          <button onTouchTap={this.startRecording} type="button">Start</button>
-          <button onTouchTap={this.stopRecording} type="button">Stop</button>
-        </div> */}
+        <h1>word search</h1>
       </>
     );
   }
